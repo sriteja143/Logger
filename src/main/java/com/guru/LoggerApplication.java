29b1a55c9f306548a43c8d49745f53c06fc4a34e
guruteja <guruteja@olivecrypto.com>
2020-10-21 17:45:03 +0530

    second commit

--- src/main/java/com/guru/LoggerApplication.java
+++ src/main/java/com/guru/LoggerApplication.java
@@ -3,7 +3,7 @@ package com.guru;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class LoggerApplication {
 
 	public static void main(String[] args) {
