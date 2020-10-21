018199606f1ca5d15eebabfd1bc4a7358d6d6717
guruteja <guruteja@olivecrypto.com>
2020-10-21 17:43:24 +0530

    Intial commit

---
+++ src/main/java/com/guru/service/ConsoleLoggerService.java
@@ -0,0 +1,18 @@
+package com.guru.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ConsoleLoggerService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	public void consolelog() {
+		logger.info("From ConsoleLoggerService");
+		logger.debug("From ConsoleLoggerService");
+		logger.warn("From ConsoleLoggerService");
+		logger.error("From ConsoleLoggerService");
+	}
+}
