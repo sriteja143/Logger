0bece415825f43fade50a5dafe5a834a7f03898b
sriteja143 <51467028+sriteja143@users.noreply.github.com>
2020-12-07 11:27:32 +0530

    Update ConsoleLoggerService.java
    test

--- src/main/java/com/guru/service/ConsoleLoggerService.java
+++ src/main/java/com/guru/service/ConsoleLoggerService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public class ConsoleLoggerService {
+public class ConsoleLoggerService { 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	public void consolelog() {
